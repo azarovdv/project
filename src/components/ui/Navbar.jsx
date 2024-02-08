17d6b7fbd35072112566d20fe2eb6390cf573720
Yusager <yusager9@gmail.com>
2024-02-08 14:23:02 +0300

    navbar2

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -13,10 +13,13 @@ export default function Navbar() {
       </style>
       <nav className="navbar navbar-expand-lg bg-success">
         <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a className="nav-link" style={{ marginLeft: '100px' }} aria-current="page" href="/">Home</a>
+                <a className="nav-link" aria-current="page" href="/">Home</a>
               </li>
             </ul>
             <ul className="navbar-nav mx-auto">
@@ -26,7 +29,7 @@ export default function Navbar() {
             </ul>
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link" style={{ marginRight: '100px' }} href="/price">Pricing</a>
+                <a className="nav-link" href="/price">Pricing</a>
               </li>
             </ul>
           </div>
@@ -35,3 +38,28 @@ export default function Navbar() {
     </>
   );
 }
+
+{ /* <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav> */ }
