d9d8fb07744d5cdd1bf279cc1d798684ce9c2b6c
DmitryZhuck <RammDima@yandex.ru>
2024-02-09 10:32:21 +0300

    slider 1

--- src/components/ui/SliderPhoto.jsx
+++ src/components/ui/SliderPhoto.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
-export default function SliderPhoto() {
+function SliderPhoto({ oneAnimal }) {
+  const styleSliderContainer = {
+    minHeight: '200px',
+    minWidth: '200px',
+  };
+  const imgStyle = {
+    padding: '0',
+    width: '296px',
+    height: '260px',
+    objectFit: 'cover',
+    // margin: '10px 0',
+    borderTopLeftRadius: '12px',
+    borderTopRightRadius: '12px',
+
+  };
+
   return (
-    <div>SliderPhoto</div>
-  )
+    <div style={styleSliderContainer}>
+      <Carousel>
+        {oneAnimal.Photos.map((photo, index) => (
+          <Carousel.Item key={index}>
+            <img style={imgStyle} src={photo.path} alt={`Animal ${index + 1}`} />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
 }
+
+export default SliderPhoto;
+
+// {
+/* <Carousel {...additionalSettings}>
+        {images.map((image, index) => (
+          <Carousel.Item key={index}>
+            <img className="d-block w-100 " src={image} alt={`Slide ${index + 1}`} />
+          </Carousel.Item>
+        ))}
+      </Carousel> */
+// }
